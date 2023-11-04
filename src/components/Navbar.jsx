0650282753812b2510fdb32806e2be1d3ad1b0d7
tolipov <ziyovuddin.developer@gmail.com>
2023-11-04 22:39:31 +0500

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,18 @@
 import logo from "../assets/logo.svg"
 export default function Navbar() {
   return (
-    <nav className="py-10 text-white">
-        <div className='flex max-w-[1400px] mx-auto items-center gap-2 font-semibold'>
-            <img src={logo} alt="logo of helloween" />
-            <h2>SPOOKY HOLIDAY</h2>
-        </div>
+    <nav className="py-10 text-white  w-full flex max-w-[1400px] mx-auto justify-between">
+      <div className='flex items-center gap-2 font-semibold'>
+        <img src={logo} alt="logo of helloween" />
+        <h2>SPOOKY HOLIDAY</h2>
+      </div>
+      <ul className="flex gap-7">
+        <li>ABOUT EVENT</li>
+        <li>RULES</li>
+        <li>GALLERY</li>
+        <li>PRICES</li>
+        <li>CONTACT</li>
+      </ul>
     </nav>
   )
 }
